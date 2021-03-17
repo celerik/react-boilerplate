41a79f8af8ce2cc898778d4e796001bbdc963065
Diego Assia <diegoassia@gmail.com>
2021-03-17 12:13:00 -0500

    Feat(Create setup) Initial setup folder and configuration

--- source/src/pages/Dashboard/index.js
+++ source/src/pages/Dashboard/index.js
@@ -1,69 +1,19 @@
+// @packages
 import React from 'react';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
 
-import styles from './styles'
+// @styles
+import styles from './styles';
+
+// @scripts
 import { config } from '../../config'
 
-const Dashboard = ({classes}) => {
-  const [state, setState] = React.useState({
-    gilad: true,
-    jason: false,
-    antoine: false,
-  });
-
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
-
-  const { gilad, jason, antoine } = state;
-  const error = [gilad, jason, antoine].filter((v) => v).length !== 2;
-
-  return (
+const Dashboard = ({classes}) => (
     <div className={classes.root}>
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">{config.text.dashboardPage.helloWorld}</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
-            label="Gilad Gray"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
-            label="Jason Killian"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
-            label="Antoine Llorca"
-          />
-        </FormGroup>
-        <FormHelperText>Be careful</FormHelperText>
-      </FormControl>
-      <FormControl required error={error} component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">Pick two</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
-            label="Gilad Gray"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
-            label="Jason Killian"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
-            label="Antoine Llorca"
-          />
-        </FormGroup>
-        <FormHelperText>You can display an error</FormHelperText>
-      </FormControl>
+        <Typography >{config.text.dashboardPage.helloWorld}</Typography>
+        
     </div>
   );
-}
 
 export default withStyles(styles)(Dashboard)
--- source/src/pages/Dashboard/styles.js
+++ source/src/pages/Dashboard/styles.js
@@ -1,12 +1,6 @@
 
 import globals from '../../styles/global';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default theme => Object.assign({}, globals(theme), {
-    root: {
-        display: 'flex',
-    },
-    formControl: {
-        margin: theme.spacing(3)
-    },
+    
 });
