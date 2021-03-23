4391346eba19230fc2efea77729b36c89827d59f
Diego Assia <diegoassia@gmail.com>
2021-03-23 12:32:20 -0500

    Feat(Setup test suite) Solve comments

--- source/src/test/actions/user.test.js
+++ source/src/test/actions/user.test.js
@@ -7,33 +7,37 @@ import {
     logout
 } from '../../actions';
 
-test('login (http request success)', () => {
-    const credentials = {
-        email: config.settings.serviceMocker.loginUserName,
-        password: config.settings.serviceMocker.loginPassword
-    };
-    const actionCreator = login(credentials);
-    const expectedActions = [{
-        type: LOGIN,
-        payload: config.mockData.security.user
-    }];
-    return global.testDispatch(actionCreator, expectedActions);
+describe('login', () => {
+    test('login (http request success)', () => {
+        const credentials = {
+            email: config.settings.serviceMocker.loginUserName,
+            password: config.settings.serviceMocker.loginPassword
+        };
+        const actionCreator = login(credentials);
+        const expectedActions = [{
+            type: LOGIN,
+            payload: config.mockData.security.user
+        }];
+        return global.testDispatch(actionCreator, expectedActions);
+    });
+
+    test('login (http request fails)', () => {
+        const credentials = {
+            userName: config.settings.serviceMocker.loginUserName,
+            password: config.settings.serviceMocker.loginPassword
+        };
+        const actionCreator = login(credentials);
+        const expectedActions = [];
+        return global.testDispatchWithNetworkError(actionCreator, expectedActions);
+    });
 });
 
-test('login (http request fails)', () => {
-    const credentials = {
-        userName: config.settings.serviceMocker.loginUserName,
-        password: config.settings.serviceMocker.loginPassword
-    };
-    const actionCreator = login(credentials);
-    const expectedActions = [];
-    return global.testDispatchWithNetworkError(actionCreator, expectedActions);
-});
-
-test('logout', () => {
-    const actionCreator = logout();
-    const expectedActions = [{
-        type: LOGOUT
-    }];
-    return global.testDispatch(actionCreator, expectedActions);
+describe('logout', () => {
+    test('logout', () => {
+        const actionCreator = logout();
+        const expectedActions = [{
+            type: LOGOUT
+        }];
+        return global.testDispatch(actionCreator, expectedActions);
+    });
 });
--- source/src/test/reducers/user.test.js
+++ source/src/test/reducers/user.test.js
@@ -3,32 +3,33 @@ import { LOGIN } from '../../actions';
 import { config } from '../../config';
 import { userReducer } from '../../reducers/user';
 
-test('userReducer: LOGIN', () => {
-    const payload = config.mockData.security.user;
-    const action = {
-        type: LOGIN,
-        payload
-    };
-    const newState = userReducer(config.initialState.user, action);
-    const expectedState = Object.assign({}, config.initialState.user, {
-        account: {
-            authToken: payload.authToken,
-            email: payload.email,
-            name: payload.name,
-            id: payload.id,
-            password: payload.password,
-            userName: payload.userName
-        }
+describe('userReducer', () => {
+    test('userReducer: LOGIN', () => {
+        const payload = config.mockData.security.user;
+        const action = {
+            type: LOGIN,
+            payload
+        };
+        const newState = userReducer(config.initialState.user, action);
+        const expectedState = Object.assign({}, config.initialState.user, {
+            account: {
+                authToken: payload.authToken,
+                email: payload.email,
+                name: payload.name,
+                id: payload.id,
+                password: payload.password,
+                userName: payload.userName
+            }
+        });
+        expect(newState).toEqual(expectedState);
     });
-    expect(newState).toEqual(expectedState);
-});
 
-test('userReducer: DEFAULT', () => {
-    const initialState = config.initialState.user;
-
-    const action = {
-        type: 'UNLISTENED_ACTION'
-    };
-    const newState = userReducer(initialState, action);
-    expect(newState).toEqual(initialState);
+    test('userReducer: DEFAULT', () => {
+        const initialState = config.initialState.user;
+        const action = {
+            type: 'UNLISTENED_ACTION'
+        };
+        const newState = userReducer(initialState, action);
+        expect(newState).toEqual(initialState);
+    });
 });
