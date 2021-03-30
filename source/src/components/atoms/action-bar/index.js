1177d8779115e8590a9a7c196b6b6b22267edc56
Santiago <scb9908@gmail.com>
2021-03-30 00:35:05 -0500

    Created action bar component

--- source/src/components/atoms/action-bar/index.js
+++ source/src/components/atoms/action-bar/index.js
@@ -1,23 +1,74 @@
 // @packages
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withStyles } from '@material-ui/core';
 
 // @scripts
 import styles from './styles';
+import { dimensions } from '../../../styles/global';
 
-const Component = ({
-    id
+const ActionBar = ({
+    classes,
+    backgroundColor,
+    bottomActions,
+    onClickAway,
+    onClickInside,
+    topActions,
+    id,
+    width
 }) => (
-    <div>
-        {id}
-    </div>
+    <ClickAwayListener onClickAway={onClickAway}>
+        <Paper
+            className={classes.mainContainer}
+            onClick={onClickInside}
+            id={id}
+            style={{
+                backgroundColor,
+                width
+            }}
+        >
+            <div className={classes.topActions}>
+                {topActions.map(actionComponent => (
+                    React.cloneElement(actionComponent)
+                ))}
+            </div>
+            <div>
+                {bottomActions.map(actionComponent => (
+                    React.cloneElement(actionComponent)
+                ))}
+            </div>
+        </Paper>
+    </ClickAwayListener>
 );
 
-Component.propTypes = {
-    id: PropTypes.string.isRequired
+ActionBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+    backgroundColor: PropTypes.string,
+    bottomActions: PropTypes.arrayOf(PropTypes.shape({
+        icon: PropTypes.node,
+        label: PropTypes.string,
+        onClick: PropTypes.func
+    })),
+    id: PropTypes.string.isRequired,
+    onClickAway: PropTypes.func,
+    onClickInside: PropTypes.func,
+    topActions: PropTypes.arrayOf(PropTypes.shape({
+        icon: PropTypes.node,
+        label: PropTypes.string,
+        onClick: PropTypes.func
+    })),
+    width: PropTypes.number
 };
 
-Component.defaultProps = {};
+ActionBar.defaultProps = {
+    backgroundColor: null,
+    bottomActions: [],
+    onClickAway: Function.prototype,
+    onClickInside: Function.prototype,
+    topActions: [],
+    width: dimensions.MAIN_MENU_COLLAPSED_WIDTH
+};
 
-export default withStyles(styles)(Component);
+export default withStyles(styles)(ActionBar);
---
+++ source/src/components/atoms/action-bar/styles.js
@@ -0,0 +1,14 @@
+export default theme => ({
+    mainContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'space-between',
+        padding: '20px 0'
+    },
+    topActions: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column'
+    }
+});
