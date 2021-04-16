5440240e661206ad63bc960a250baf39343fc835
Diego Andres Cardona Henao <diegocardona239547@correo.itm.edu.co>
2021-04-16 14:16:28 -0500

    Changes resolved according to Figma

--- source/src/components/organisms/clone-project-dialog/styles.js
+++ source/src/components/organisms/clone-project-dialog/styles.js
@@ -1,17 +1,19 @@
 export default () => ({
+    acctionContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        paddingBottom: 0,
+        radius: 5,
+        width: 434
+    },
     adjustText: {
         color: 'gray',
         display: 'flex',
+        fontSize: 15,
         margin: 'auto',
+        paddingTop: 0,
         textAlign: '-webkit-center',
         width: '100%'
-    },
-    acctionContainer: {
-        width: 434,
-        display: 'flex',
-        justifyContent: 'center',
-        paddingBottom: 20,
-        radius: 5
     },
     backdropClassName: {
         backgroundColor: 'rgba(254, 254, 254, 0.1)',
@@ -20,29 +22,29 @@ export default () => ({
     button: {
         border: '1px solid mediumpurple',
         color: '#161c3a',
+        margin: 10,
+        marginBottom: 21,
         padding: '8px 15px 8px 15px',
         textTransform: 'capitalize',
-        width: '40',
-        margin: 10,
-        marginBottom: 0,
-        marginTop: 0
+        width: '40'
     },
     closeButton: {
+        color: '#293131',
         marginRight: 5,
         marginTop: 10,
         padding: 3,
-        color: '#293131',
         position: 'absolute',
         right: 0
     },
     titleHeader: {
-        marginTop: 5,
         alignItems: 'center',
         color: 'black',
         display: 'flex',
         justifyContent: 'center',
+        paddingTop: 25,
         '& .MuiTypography-h6': {
             fontSize: 'x-large',
+            fontWeight: 'bolder',
             fontWeight: 600
         }
     }
