71d3771d21aee60a1a8e14ed7afb515c8b71d129
Santiago <scb9908@gmail.com>
2021-04-20 01:51:08 -0500

    Login screen implemented and routes permission

--- source/src/components/templates/login/styles.js
+++ source/src/components/templates/login/styles.js
@@ -1,5 +1,23 @@
-// @packages
-import globals from '../../../styles/global';
-
-export default theme => Object.assign({}, globals(theme), {
+export default theme => ({
+    mainContainer: {
+        alignItems: 'center',
+        display: 'flex',
+        height: '100vh',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        backgroundColor: theme.palette.background.secondary,
+        opacity: 0.8,
+        zIndex: 1
+    },
+    content: {
+        position: 'relative',
+        zIndex: 1
+    }
 });
